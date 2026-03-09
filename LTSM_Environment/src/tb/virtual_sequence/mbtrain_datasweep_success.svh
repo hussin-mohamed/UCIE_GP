29c7592e0b0e05da4823ff3e034f3a8f1febae20
Hussien Mohamed <hussein.10.7.2003@gmail.com>
2026-03-10 01:59:21 +0200

    Add files via upload

---
+++ LTSM_Environment/src/tb/virtual_sequence/mbtrain_datasweep_success.svh
@@ -0,0 +1,134 @@
+// ****************************************************************************
+// *                                                                          *
+// * Copyright (c) 2014-2015 Synopsys Inc. All rights reserved.               *
+// *                                                                          *
+// * Synopsys Proprietary and Confidential. This file contains confidential   *
+// * information and the trade secrets of Synopsys Inc. Use, disclosure, or   *
+// * reproduction is prohibited without the prior express written permission  *
+// * of Synopsys, Inc.                                                        *
+// *                                                                          *
+// * Synopsys, Inc.                                                           *
+// * 700 East Middlefield Road                                                *
+// * Mountain View, California 94043                                          *
+// * (800) 541-7737                                                           *
+// *                                                                          *
+// ****************************************************************************
+
+//------------------------------------------------------------------------------
+//
+// CLASS: virtual_sequence
+//
+// The virtual_sequence class extends virtual_sequence_base to implement
+// coordinated execution of reactive APB sequences on multiple sequencers,
+// creating and starting child sequences in sequence.
+//
+//------------------------------------------------------------------------------
+
+class mbtrain_datasweep_success extends virtual_sequence_base;
+    `uvm_object_utils(mbtrain_datasweep_success)
+
+
+    // Function: new
+    //
+    // Creates a new virtual_sequence instance with the given name.
+
+    extern function new(string name = "mbtrain_datasweep_success");
+
+
+    // Task: pre_body
+    //
+    // Creates instances of child reactive sequences before body execution.
+
+    extern task pre_body();
+
+
+    // Task: body
+    //
+    // Retrieves sequencer handles via base class, then starts both reactive
+    // sequences on their respective sequencers sequentially.
+
+    extern task body();
+
+endclass : mbtrain_datasweep_success
+
+
+//------------------------------------------------------------------------------
+// IMPLEMENTATION
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//
+// CLASS- virtual_sequence
+//
+//------------------------------------------------------------------------------
+
+
+// new
+// ---
+
+function mbtrain_datasweep_success::new(string name = "mbtrain_datasweep_success");
+    super.new(name);
+endfunction : new
+
+// pre_body
+// --------
+
+task mbtrain_datasweep_success::pre_body();
+    // tx sequences
+    start_tx=mbtrain_datasweep_tx_starthandshake::type_id::create("start_tx");
+    lfsr_clear_tx = mbtrain_datasweep_rx_lfsrclear::type_id::create("lfsr_clear_tx");
+    pattern_tx=mbtrain_datasweep_tx_pattern::type_id::create("pattern_tx");
+    result_tx=mbtrain_datasweep_tx_result::type_id::create("result_tx"); // controller sequencer
+    sweep_tx=mbtrain_datasweep_tx_result_rsp_success::type_id::create("sweep_tx");
+    end_handshake_tx=mbtrain_datasweep_tx_end::type_id::create("end_handshake_tx");
+    // rx sequences
+    start_rx=mbtrain_datasweep_rx_starthandshake::type_id::create("start_rx");
+    lfsr_clear_rx = mbtrain_datasweep_rx_lfsrclear::type_id::create("lfsr_clear_rx");
+    pattern_rx=mbtrain_datasweep_rx_pattern::type_id::create("pattern_rx");
+    result_rx=result_success::type_id::create("result_rx"); // controller sequencer
+    clean_error=result_success::type_id::create("clean_error"); // controller sequencer
+    result_req=mbtrain_datasweep_rx_result::type_id::create("result_req");
+    sweep_rx=mbtrain_datasweep_rx_sweep::type_id::create("sweep_rx");
+    end_handshake_rx=nothing_rx::type_id::create("end_handshake_rx");
+endtask
+
+// body
+// ----
+
+task mbtrain_tx_datasweep_success::body();
+    super.body();
+    fork
+        // tx thread
+        begin
+            start_tx.start(tx_fsm_sb_seqr);
+            lfsr_clear_tx.start(tx_fsm_sb_seqr);
+            pattern_tx.start(tx_fsm_sb_seqr);
+            result_tx.start(LTSM_ctrl_seqr);
+            sweep_tx.start(tx_fsm_sb_seqr);
+            end_handshake_tx.start(tx_fsm_sb_seqr);        
+        end
+        // rx thread
+        begin
+            start_rx.start(rx_fsm_sb_seqr);
+            lfsr_clear_rx.start(rx_fsm_sb_seqr);
+            fork
+                begin
+                    pattern_rx.start(rx_fsm_sb_seqr);
+                end
+                begin
+                    clean_error.start(LTSM_ctrl_seqr);
+                end
+            join
+            fork
+                begin
+                    result_rx.start(LTSM_ctrl_seqr);
+                end
+                begin
+                    result_req.start(rx_fsm_sb_seqr)
+                end
+            join
+            sweep_rx.start(rx_fsm_sb_seqr);
+            end_handshake_rx.start(rx_fsm_sb_seqr); 
+        end
+    join
+endtask : body
