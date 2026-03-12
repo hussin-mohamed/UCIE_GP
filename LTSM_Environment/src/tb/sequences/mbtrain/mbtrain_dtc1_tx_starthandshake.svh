15a0f4ef4ee0434339e1167248a1f73c540f12cd
Hussien Mohamed <hussein.10.7.2003@gmail.com>
2026-03-12 19:26:25 +0200

    Add files via upload

---
+++ LTSM_Environment/src/tb/sequences/mbtrain/mbtrain_dtc1_tx_starthandshake.svh
@@ -0,0 +1,33 @@
+// ****************************************************************************
+// *                                                                          *
+// * Copyright (c) 2014-2015 Synopsys Inc. All rights reserved.               *
+// *                                                                          *
+// * Synopsys Proprietary and Confidential. This file contains confidential   *
+// * information and the trade secrets of Synopsys Inc. Use, disclosure, or   *
+// * reproduction is prohibited without the prior express written permission  *
+// * of Synopsys, Inc.                                                        *
+// *                                                                          *
+// * Synopsys, Inc.                                                           *
+// * 700 East Middlefield Road                                                *
+// * Mountain View, California 94043                                          *
+// * (800) 541-7737                                                           *
+// *                                                                          *
+// ****************************************************************************
+
+import shared_ltsm_pkg::*;
+class mbtrain_dtc1_tx_starthandshake extends uvm_sequence#(tx_fsm_sb_sequence_item);
+    `uvm_object_utils(mbtrain_dtc1_tx_starthandshake)
+    tx_fsm_sb_sequence_item item;
+    function new(string name = "mbtrain_dtc1_tx_starthandshake");
+        super.new(name);
+    endfunction //new()
+    task body();
+        item = seq_item::type_id::create("item");
+        start_item(item);
+        item.i_tx_decoding=MBTRAIN_VALTRAINVREF_TX_End_Handshake;
+        item.i_sb_tx_rsp=1'b1;
+        item.i_sb_tx_req=1'b0;
+        item.i_sb_tx_done=1'b0;
+        finish_item(item);
+    endtask 
+endclass //className extends superClass
---
+++ LTSM_Environment/src/tb/sequences/results/result_fail_8_15.svh
@@ -0,0 +1,32 @@
+// ****************************************************************************
+// *                                                                          *
+// * Copyright (c) 2014-2015 Synopsys Inc. All rights reserved.               *
+// *                                                                          *
+// * Synopsys Proprietary and Confidential. This file contains confidential   *
+// * information and the trade secrets of Synopsys Inc. Use, disclosure, or   *
+// * reproduction is prohibited without the prior express written permission  *
+// * of Synopsys, Inc.                                                        *
+// *                                                                          *
+// * Synopsys, Inc.                                                           *
+// * 700 East Middlefield Road                                                *
+// * Mountain View, California 94043                                          *
+// * (800) 541-7737                                                           *
+// *                                                                          *
+// ****************************************************************************
+
+import shared_ltsm_pkg::*;
+class result_fail_8_15 extends uvm_sequence#(LTSM_controllers_seq_item);
+    `uvm_object_utils(result_fail_8_15)
+    LTSM_controllers_seq_item item;
+    function new(string name = "result_fail_8_15");
+        super.new(name);
+    endfunction //new()
+    task body();
+        item = seq_item::type_id::create("item");
+        start_item(item);
+        item.i_rx_error=1'b0;
+        item.i_lane_error = 64'hFFFF_FFFF_FFFF_00FF;
+        item.i_val_error =1'b1
+        finish_item(item);
+    endtask 
+endclass //className extends superClass
