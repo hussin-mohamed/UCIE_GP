dca1b9e3aef760d438cb5f3d158292a9256daeac
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-03-14 00:17:20 +0200

    Refactor doSpecificCombAction for linkinit_state_rx

--- LTSM_Environment/src/tb/model/active_rx.svh
+++ LTSM_Environment/src/tb/model/active_rx.svh
@@ -42,14 +42,18 @@
      
     virtual function bit doSpecificCombAction(FSMContext cntxt, LTSM_controllers_seq_item ctrl_item , ltsm_rdi_sequence_item rdi_item ,rx_fsm_sb_sequence_item  rx_sb_item ,tx_fsm_sb_sequence_item tx_sb_item); 
     o_rx_encoding_exp = RX_ACTIVE_Active ;
-        if(o_rx_encoding_exp == ctrl_item.o_rx_encoding)
-            match = 1'b1;
-        else 
-        begin
-          `uvm_info("active_state_rx", $sformatf("Expected o_rx_encoding: %b, Actual o_rx_encoding: %b", o_rx_encoding_exp, ctrl_item.o_rx_encoding), UVM_LOW);
-            match = 1'b0;
-        end 
-            return match;
+        if(ctrl_item.i_rx_decoding == ACTIVE_LINKINIT_TX_State_Req_Handshake  && rx_sb_item.i_sb_rx_done && cntxt.currentstate_rx == linkinit_state_rx::instance()) 
+      begin
+        o_rx_encoding_exp = RX_ACTIVE_Active ;
+            if(o_rx_encoding_exp == ctrl_item.o_rx_encoding)
+                match = 1'b1;
+            else
+            begin
+              `uvm_info("linkinit_state_rx", $sformatf("Expected o_rx_encoding: %b, Actual o_rx_encoding: %b", o_rx_encoding_exp, ctrl_item.o_rx_encoding), UVM_LOW);
+                match = 1'b0;
+            end
+      end  
+        return match;
     endfunction 
  
  
