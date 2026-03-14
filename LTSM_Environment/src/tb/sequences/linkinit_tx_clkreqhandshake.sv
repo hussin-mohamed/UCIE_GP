feb63b8c56dcc96c734b34952a9fe0e16b0de4cf
MahmoudHesham22 <168492061+MahmoudHesham22@users.noreply.github.com>
2026-03-14 05:13:06 +0200

    Add files via upload

---
+++ LTSM_Environment/src/tb/sequences/linkinit_tx_clkreqhandshake.sv
@@ -0,0 +1,36 @@
+// ****************************************************************************
+// *                                                                          *
+// * Copyright (c) 2014-2015 Synopsys Inc. All rights reserved.               *
+// *                                                                          *
+// * Synopsys Proprietary and Confidential. This file contains confidential   *
+// * information and the trade secrets of Synopsys Inc. Use, disclosure, or   *
+// * reproduction is prohibited without the prior express written permission  *
+// * of Synopsys, Inc.                                                        *
+// *                                                                          *
+// * Synopsys, Inc.                                                           *
+// * 700 East Middlefield Road                                                *
+// * Mountain View, California 94043                                          *
+// * (800) 541-7737                                                           *
+// *                                                                          *
+// ****************************************************************************
+
+import shared_ltsm_pkg::*;
+class linkinit_tx_clk_req_handshake extends uvm_sequence#(tx_fsm_sb_sequence_item);
+    `uvm_object_utils(linkinit_tx_clk_req_handshake)
+    tx_fsm_sb_sequence_item item;
+    
+    function new(string name = "linkinit_tx_clk_req_handshake");
+        super.new(name);
+    endfunction //new()
+    
+    task body();
+         item = seq_item::type_id::create("item");
+        start_item(item);
+        item.i_tx_decoding      = RX_MBTRAIN_LINKSPEED_Send_Done_RESP;
+        item.i_sb_tx_rsp        = 1'b1;
+        item.i_sb_tx_req        = 1'b0;
+        item.i_sb_tx_done       = 1'b0;
+        finish_item(item);
+    endtask 
+endclass //className extends superClass
+
