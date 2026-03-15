534a1bc1fe639a57645994a53bb171a41f7c603b
Mahmoud Hussein <mahmoudhussein1227@gmail.com>
2026-03-15 20:02:15 +0200

    Update mbinit_param_state_tx.sv

--- LTSM_Environment/src/tb/model/mbinit_param_state_tx.sv
+++ LTSM_Environment/src/tb/model/mbinit_param_state_tx.sv
@@ -14,14 +14,16 @@
 // *                                                                          *
 // ****************************************************************************
 
+import shared_ltsm_pkg::*;
+
 class MbInitParamState_tx extends State;
-   import shared_ltsm_pkg::*;
    `uvm_object_utils(MbInitParamState_tx)
 
    static MbInitParamState_tx inst;
 
    logic [8:0] o_tx_encoding_exp;
    logic o_tx_sb_req_exp;
+   logic [15:0] o_tx_info_exp;
    bit match;
 
    protected function new(string name = "MbInitParamState_tx");
@@ -36,10 +38,11 @@ class MbInitParamState_tx extends State;
 
    virtual function bit doSpecificCombAction(FSMContext cntxt,LTSM_controllers_sequence_item item_controllers_in,ltsm_rdi_sequence_item item_rdi_in,rx_fsm_sb_sequence_item item_rx_fsm_sb_in,tx_fsm_sb_sequence_item item_tx_fsm_sb_in,
                                               LTSM_controllers_sequence_item item_controllers_out,ltsm_rdi_sequence_item item_rdi_out,rx_fsm_sb_sequence_item item_rx_fsm_sb_out,tx_fsm_sb_sequence_item item_tx_fsm_sb_out);
-      if(item_controllers_in.tx_decoding == SBINIT_TX_Done_Handshake && cntxt.current_state_tx == SbInitState_tx::Instance() && item_tx_fsm_sb_in.i_sb_tx_done) begin
+      if(item_controllers_in.tx_decoding == SBINIT_TX_Done_Handshake && cntxt.current_state_tx == SbInitState_tx::Instance() && item_tx_fsm_sb_in.i_sb_tx_rsp) begin
          o_tx_encoding_exp = 'h10;
          o_tx_sb_req_exp = 1;
-         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp)
+         o_tx_info_exp = 0;
+         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp && item_tx_fsm_sb_out.o_tx_info == o_tx_info_exp)
             match = 1;
          else
             match = 0;
