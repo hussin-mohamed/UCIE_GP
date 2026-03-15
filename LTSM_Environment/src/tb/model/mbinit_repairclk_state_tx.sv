6d075e96b8704fe4e99f8ab09d60682d459d32bb
Mahmoud Hussein <mahmoudhussein1227@gmail.com>
2026-03-15 20:07:15 +0200

    Update mbinit_repairclk_state_tx.sv

--- LTSM_Environment/src/tb/model/mbinit_repairclk_state_tx.sv
+++ LTSM_Environment/src/tb/model/mbinit_repairclk_state_tx.sv
@@ -16,10 +16,12 @@
 
 import shared_ltsm_pkg::*;
 class MbInitRepairClkState_tx extends State;
+  
    `uvm_object_utils(MbInitRepairClkState_tx)
 
    static MbInitRepairClkState_tx inst;
    logic o_tx_sb_req_exp;
+   logic [15:0] o_tx_info_exp;
 
    logic [8:0] o_tx_encoding_exp;
    bit match;
@@ -36,11 +38,12 @@ class MbInitRepairClkState_tx extends State;
 
    virtual function bit doSpecificCombAction(FSMContext cntxt,LTSM_controllers_sequence_item item_controllers_in,ltsm_rdi_sequence_item item_rdi_in,rx_fsm_sb_sequence_item item_rx_fsm_sb_in,tx_fsm_sb_sequence_item item_tx_fsm_sb_in,
                                               LTSM_controllers_sequence_item item_controllers_out,ltsm_rdi_sequence_item item_rdi_out,rx_fsm_sb_sequence_item item_rx_fsm_sb_out,tx_fsm_sb_sequence_item item_tx_fsm_sb_out);
-      if(cntxt.current_state_tx == MbInitCalState_tx::Instance() && item_controllers_in.i_tx_decoding == MBINIT_CAL_TX_Done_Handshake && item_tx_fsm_sb_in.i_sb_tx_done == 1'b1) begin
+      if(cntxt.current_state_tx == MbInitCalState_tx::Instance() && item_controllers_in.i_tx_decoding == MBINIT_CAL_TX_Done_Handshake && item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1) begin
          o_tx_encoding_exp = 'h20;
          // init req 
          o_tx_sb_req_exp = 1;
-         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp)
+         o_tx_info_exp = 0;
+         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp && item_tx_fsm_sb_out.o_tx_info == o_tx_info_exp)
             match = 1;
          else
             match = 0;
@@ -57,22 +60,27 @@ class MbInitRepairClkState_tx extends State;
          o_tx_encoding_exp = 'h22;
          // result req
          o_tx_sb_req_exp = 1; 
-         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp)
+         o_tx_info_exp = 0;
+         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp && item_tx_fsm_sb_out.o_tx_info == o_tx_info_exp)
             match = 1;
          else
             match = 0;
       end
-      else if(item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_controllers_in.i_tx_decoding == 'h22 && item_tx_fsm_sb_in.i_tx_data == ERROR_DETECTED) begin
+      // failed clk lanes -> trainerror hs
+      else if(item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_controllers_in.i_tx_decoding == 'h22 && &item_tx_fsm_sb_in.i_tx_info[2:0] == 3'b000) begin
          o_tx_encoding_exp = 'hE0;
          if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp)
             match = 1;
          else
             match = 0;
       end
-      else if(item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_controllers_in.i_tx_decoding == 'h22 && item_tx_fsm_sb_in.i_tx_data == NO_ERRORS) begin
+      // successful clk lanes
+      else if(item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_controllers_in.i_tx_decoding == 'h22 && &item_tx_fsm_sb_in.i_tx_info[2:0]  == 3'b111) begin
          o_tx_encoding_exp = 'h23;
          // done handshake req
          o_tx_sb_req_exp = 1;
+         o_tx_info_exp = 0;
+
          if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp)
             match = 1;
          else
