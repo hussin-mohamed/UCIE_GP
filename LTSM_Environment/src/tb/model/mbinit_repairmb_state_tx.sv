1bc1fe71eaca744bc0159d44d7cfd12ae1ca8d92
Mahmoud Hussein <mahmoudhussein1227@gmail.com>
2026-03-15 20:09:40 +0200

    Update mbinit_repairmb_state_tx.sv

--- LTSM_Environment/src/tb/model/mbinit_repairmb_state_tx.sv
+++ LTSM_Environment/src/tb/model/mbinit_repairmb_state_tx.sv
@@ -16,10 +16,12 @@
 
 import shared_ltsm_pkg::*;
 class MbInitRepairMbState_tx extends State;
+   
    `uvm_object_utils(MbInitRepairMbState_tx)
 
    static MbInitRepairMbState_tx inst;
    logic o_tx_sb_req_exp;
+   logic [15:0] o_tx_info_exp;
 
    logic [8:0] o_tx_encoding_exp;
    bit match;
@@ -39,7 +41,7 @@ class MbInitRepairMbState_tx extends State;
       // Lane repair negotiation
 
       // start the lane repair
-      if(item_controllers_in.i_tx_decoding == MBINIT_REVERSAL_TX_Done_Handshake && item_tx_fsm_sb_in.i_sb_tx_done == 1'b1) begin
+      if(item_controllers_in.i_tx_decoding == MBINIT_REVERSAL_TX_Done_Handshake && item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1) begin
          o_tx_encoding_exp = 'h38;
          o_tx_sb_req_exp = 1;
          if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp)
@@ -51,15 +53,18 @@ class MbInitRepairMbState_tx extends State;
       // point test 
       else if(item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_controllers_in.i_tx_decoding == 'h38) begin
          o_tx_encoding_exp = 'h39;
-         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp)
+         item_tx_sb_req_exp = 1;
+         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp)
             match = 1;
          else
             match = 0;
       end
 
-      // apply degarding
-      else if(item_controllers_in.i_tx_done && item_controllers_in.i_tx_decoding == 'h39) begin
-         o_tx_encoding_exp = 'h3A;
+      //point test fsm
+      // ************************************************************************************
+      // start point test
+      else if(item_controllers_in.i_tx_rsp && item_controllers_in.i_tx_decoding == 'h39) begin
+         o_tx_encoding_exp = 'h180;
          o_tx_sb_req_exp = 1;
          if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp)
             match = 1;
@@ -67,8 +72,77 @@ class MbInitRepairMbState_tx extends State;
             match = 0;
       end
 
+      // lfsr clear hs
+      else if(item_controllers_in.i_tx_decoding == 'h180  && item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1) begin
+         o_tx_encoding_exp = 'h181;
+         o_tx_sb_req_exp = 1;
+         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp)
+            match = 1;
+         else
+            match = 0;
+      end
+
+      //pattern generation
+      else if (item_controllers_in.i_tx_decoding == 'h181 && item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1) begin
+         o_tx_encoding_exp = 'h182;
+         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp)
+            match = 1;
+         else
+            match = 0;
+      end
+
+      //result handshake
+      else if (item_controllers_in.i_tx_done && item_controllers_in.i_tx_decoding == 'h182) begin
+         o_tx_encoding_exp = 'h183;
+         o_tx_sb_req_exp = 1;
+         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp)
+            match = 1;
+         else
+            match = 0;
+      end
+
+      // end handshake + // need to check the retry state
+      else if(item_controllers_in.i_tx_decoding == 'h183 && item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_tx_fsm_sb_in.i_tx_info[4] == 'h1) begin
+         o_tx_encoding_exp = 'h184;
+         o_tx_sb_req_exp = 1;
+         o_tx_info_exp = 15'h0;
+         
+         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp && item_tx_fsm_sb_out.o_tx_info == o_tx_info_exp)
+            match = 1;
+         else
+            match = 0;
+      end
+
+      // rerty the point test if failure,  need to check the retry state as the encoding is the same as end handshake
+      else if(item_controllers_in.i_tx_decoding == 'h183 && item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_tx_fsm_sb_in.i_tx_info[4] == 'h0) begin
+         o_tx_encoding_exp = 'h181;
+         o_tx_sb_req_exp = 1;
+         o_tx_info_exp = 15'h0;
+         
+         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp && item_tx_fsm_sb_out.o_tx_info == o_tx_info_exp)
+            match = 1;
+         else
+            match = 0;
+      end
+
+
+
+
+      // *********************************************************************************************
+      // apply degarding
+      else if(item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_controllers_in.i_tx_decoding == 'h183) begin
+         o_tx_encoding_exp = 'h3A;
+         o_tx_sb_req_exp = 1;
+         // lane map code assigned in the info field
+         o_tx_info_exp[2:0] = `LANE_MAP_CODE
+         if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp)
+            match = 1;
+         else
+            match = 0;
+      end
+
       //degradeing result (FAIL)
-      else if(item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_controllers_in.i_tx_decoding == 'h3A && item_tx_fsm_sb_in.i_tx_data == NOT_ALL_CURRENT_LANES_GOOD) begin
+      else if(item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_controllers_in.i_tx_decoding == 'h3A && item_tx_fsm_sb_in.i_tx_data <= `RESULT_THRESHOLD) begin
          o_tx_encoding_exp = 'h39;
          o_tx_sb_req_exp = 1;
          if(item_controllers_out.o_tx_encoding == o_tx_encoding_exp && item_tx_fsm_sb_out.o_tx_sb_req == o_tx_sb_req_exp)
@@ -78,7 +152,7 @@ class MbInitRepairMbState_tx extends State;
       end
 
       // degareing result (PASS)
-      else if(item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_controllers_in.i_tx_decoding == 'h3A && item_tx_fsm_sb_in.i_tx_data == ALL_CURRENT_LANES_GOOD) begin
+      else if(item_tx_fsm_sb_in.i_sb_tx_rsp == 1'b1 && item_controllers_in.i_tx_decoding == 'h3A && item_tx_fsm_sb_in.i_tx_data > `RESULT_THRESHOLD) begin
          o_tx_encoding_exp = 'h3B;
          // done handshake req
          o_tx_sb_req_exp = 1;
