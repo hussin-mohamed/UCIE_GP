16a85e435ad40251c5edb7dafa83f103ed6b937f
Amr El Batarny <amrelbatarny8@gmail.com>
2026-05-03 22:38:28 +0300

    Modified the fields of the rmblink_seq_item

--- rp_env/src/tb/sequence_items/rmblink_seq_item.svh
+++ rp_env/src/tb/sequence_items/rmblink_seq_item.svh
@@ -25,17 +25,11 @@
 
 class rmblink_seq_item extends uvm_sequence_item;
 
-  rand logic          [7:0]             val_pattern;        // Serialized 8-bit pattern representing the Valid signal used during link training or active phases.
-  rand logic                            clk_stream_p [];    // Serialized 32-bit pattern representing the positive Clock signal used for clock training.
-  rand logic                            clk_stream_n [];    // Serialized 32-bit pattern representing the negative Clock signal used for clock training.
-  rand int unsigned                     idle_ui_cnt_dat;    // Specifies the number of idle Unit Intervals (UIs) to inject before or after the active/training Data transmission.
-  rand int unsigned                     idle_ui_cnt_val;    // Specifies the number of idle Unit Intervals (UIs) to inject before or after the active/training Valid transmission.
-  rand int unsigned                     idle_ui_cnt_clk;    // Specifies the number of idle Unit Intervals (UIs) to inject before or after the active/training Clok transmission.
-  rand int unsigned                     clk_iter_cnt;       // Defines the number of times the clock pattern is repeated during the training sequence.
-  rand int unsigned                     val_iter_cnt;       // Defines the number of times the valid pattern is repeated during the training sequence.
-  rand int unsigned                     dat_iter_cnt;       // Defines the number of times the main payload data pattern is repeated during the training sequence.
+  rand logic          [7:0]             val_stream   [];    // Serialized 8-bit valid stream representing the Valid signal used during link training or active phases.
+  rand logic                            clk_stream_p [];    // Serialized positive clock stream used for clock training as well as data and valid sampling.
+  rand logic                            clk_stream_n [];    // Serialized negative clock stream used for clock training as well as data and valid sampling.
+  rand int unsigned                     idle_ui_cnt;        // Specifies the number of idle Unit Intervals (UIs) to inject before or after the active/training transmission.
   rand pattern_type_t                   pattern_type;       // Selects the specific physical training or test pattern format (CLK_PATTERN, VAL_PATTERN, or DATA_PATTERN).
-  rand rate_mode_t                      rate_mode;          // Configures the operational data transfer rate mode for the current transaction (Half Rate (HR) or Quadrature Rate (QR)).
   rand logic          [pDATA_WIDTH-1:0] data [pNUM_LANES];  // Two-dimensional array storing the explicit per-lane data payload to be driven across the physical link.
 
   `uvm_object_utils_begin(rmblink_seq_item)
@@ -54,7 +48,8 @@ class rmblink_seq_item extends uvm_sequence_item;
   `uvm_object_utils_end
 
   // Function: new
-  
+  // ...
+
   extern function new(string name = "");
 
 
