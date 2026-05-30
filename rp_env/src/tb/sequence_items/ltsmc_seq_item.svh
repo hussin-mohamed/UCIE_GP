f0b25be2310983703e2f9dd8e25144843b74b5c0
Amr El Batarny <amrelbatarny8@gmail.com>
2026-05-30 07:58:41 +0300

    Fixed the monitor bug where it was monitoring D2C tests in VALTRAIN patterns

--- rp_env/src/tb/sequence_items/ltsmc_seq_item.svh
+++ rp_env/src/tb/sequence_items/ltsmc_seq_item.svh
@@ -31,8 +31,8 @@ class ltsmc_seq_item extends uvm_sequence_item;
   logic [pDATA_WIDTH-1:0] rx_data_results;  // One bit for each lane which indicates the successful detection of the LFSR pattern on that lane.
 
   `uvm_object_utils_begin(ltsmc_seq_item)
-    `uvm_field_enum       (lane_map_code_t, lane_map_code, UVM_DEFAULT | UVM_NORECORD | UVM_NOPACK | UVM_NOCOMPARE)
-    `uvm_field_enum       (rx_encoding_t,   rx_encoding,   UVM_DEFAULT | UVM_NORECORD | UVM_NOPACK | UVM_NOCOMPARE)
+    `uvm_field_enum       (lane_map_code_t, lane_map_code, UVM_DEFAULT | UVM_NORECORD | UVM_NOPACK)
+    `uvm_field_enum       (rx_encoding_t,   rx_encoding,   UVM_DEFAULT | UVM_NORECORD | UVM_NOPACK)
     `uvm_field_int        (error_threshold,                UVM_DEFAULT | UVM_NORECORD | UVM_NOPACK | UVM_NOCOMPARE)
     `uvm_field_int        (half_rate,                      UVM_DEFAULT | UVM_NORECORD | UVM_NOPACK | UVM_NOCOMPARE)
     `uvm_field_int        (rx_data_results,                UVM_DEFAULT | UVM_NORECORD | UVM_NOPACK)
