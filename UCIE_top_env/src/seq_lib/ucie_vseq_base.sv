fcb29ff0e292be35503ade3a878b571a9f917769
Amr El Batarny <amrelbatarny8@gmail.com>
2026-06-01 06:02:27 +0300

    minore changes

--- UCIE_top_env/src/seq_lib/ucie_vseq_base.sv
+++ UCIE_top_env/src/seq_lib/ucie_vseq_base.sv
@@ -67,10 +67,21 @@ class ucie_vseq_base extends uvm_sequence;
     sb_ltsm_item.set_tx_encoding(sb_shared_pkg::SBINIT_TX_Out_Of_Reset_MSG);
     send_sb_msg(sb_ltsm_item);
 
-    $display("qqqqqqqqqqqqqqqqqqqqqqqqqqq");
-    p_sequencer.tx_fifo.get(sb_ltsm_item);
-    $display("gggggggggggggggggggggggggggg");
+    p_sequencer.rx_fifo.get(sb_ltsm_item);
     `uvm_info("VSEQ", $sformatf("GOOOOOOOOOOOOOOT222\n %s", sb_ltsm_item.sprint()), UVM_LOW)
+
+    // p_sequencer.rx_fifo.get(sb_ltsm_item);
+    // `uvm_info("VSEQ", $sformatf("GOOOOOOOOOOOOOOT333\n %s", sb_ltsm_item.sprint()), UVM_LOW)
+
+    // p_sequencer.tx_fifo.get(sb_ltsm_item);
+    // `uvm_info("VSEQ", $sformatf("GOOOOOOOOOOOOOOT444\n %s", sb_ltsm_item.sprint()), UVM_LOW)
+    // sb_ltsm_item.set_tx_encoding(sb_shared_pkg::SBINIT_TX_Done_Handshake);
+    // send_sb_msg(sb_ltsm_item);
+
+    // p_sequencer.rx_fifo.get(sb_ltsm_item);
+    // `uvm_info("VSEQ", $sformatf("GOOOOOOOOOOOOOOT555\n %s", sb_ltsm_item.sprint()), UVM_LOW)
+    // sb_ltsm_item.set_rx_encoding(sb_shared_pkg::SBINIT_RX_Done_Handshake);
+    // send_sb_msg(sb_ltsm_item);
 
     // sb_ltsm_item.data        = 64'h0;
     // sb_ltsm_item.info        = 16'h0;
