597ab16d698c219653bce55f791ee3e52c1f0bc9
Omar Mohamed Araby <omararaby28@gmail.com>
2026-06-04 03:23:00 +0300

    Update ucie_vseq_base.sv

--- UCIE_top_env/src/seq_lib/ucie_vseq_base.sv
+++ UCIE_top_env/src/seq_lib/ucie_vseq_base.sv
@@ -58,8 +58,7 @@ typedef enum {
 
 typedef enum {
   IDEAL,
-  REQ_MISS,
-  RESP_MISS
+  MISS
 } missing_msg_e;
 
 
@@ -130,7 +129,6 @@ class ucie_vseq_base extends uvm_sequence;
 
   ucie_RX_D2C_vseq  ucie_RX_D2C;
   ucie_TX_D2C_vseq  ucie_TX_D2C;
-  trainerror_rdiexit trainerror_rdi_exit_vseq;
 
   LTSM_pkg::linkinit_wake_req_handshake   wake_req_handshake;
   LTSM_pkg::linkinit_state_req_handshake  state_req_handshake;
@@ -157,12 +155,11 @@ class ucie_vseq_base extends uvm_sequence;
     rmblink_lfsr_seq      = rmblink_sanity_lfsr_sequence::type_id::create("rmblink_lfsr_seq");
     ucie_RX_D2C           = ucie_RX_D2C_vseq::type_id::create("ucie_RX_D2C");
     ucie_TX_D2C           = ucie_TX_D2C_vseq::type_id::create("ucie_TX_D2C");
-    trainerror_rdi_exit_vseq  = trainerror_rdiexit::type_id::create("trainerror_rdi_exit_vseq");
     wake_req_handshake    = linkinit_wake_req_handshake::type_id::create("wake_req_handshake");
     state_req_handshake   = linkinit_state_req_handshake::type_id::create("state_req_handshake");
     rmblink_PerLaneID_seq = rmblink_sanity_PerLaneID_sequence::type_id::create("rmblink_PerLaneID_seq");
 
-    mbinit_vseq        = ucie_mbinit_bringup_vseq::type_id::create("mbinit_vseq");
+    mbinit_vseq         = ucie_mbinit_bringup_vseq::type_id::create("mbinit_vseq");
     valverf_vseq        = ucie_mbtrain_valverf_vseq::type_id::create("valverf_vseq");
     dataverf_vseq       = ucie_mbtrain_dataverf_vseq::type_id::create("valverf_vseq");
     speedidle_vseq      = ucie_mbtrain_speedidle_vseq::type_id::create("speedidle_vseq");
