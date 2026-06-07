959dec34b1e6d1c1150d682891e430d6d190551e
Omar Mohamed Araby <omararaby28@gmail.com>
2026-06-07 09:04:09 +0300

    Update ucie_vvref_till_rxcal_vseq.sv

--- UCIE_top_env/src/seq_lib/ucie_vvref_till_rxcal_vseq.sv
+++ UCIE_top_env/src/seq_lib/ucie_vvref_till_rxcal_vseq.sv
@@ -291,6 +291,98 @@ class ucie_vvref_till_rxcal_vseq extends ucie_vseq_base;
     );
     rxclkcal_vseq.start(p_sequencer);
 
+    valtraincenter_vseq.configure(
+        .D2c_mode(SUCCESS),
+        .pattern_mode(PAT_ALL_LANES_VALID),
+        .data_mode(VALID_PATTERN),
+        .info_mode(CORRECT),
+        .message_mode(ALL_LANES_VALID),
+        .valid_mode(VALID_CORRECT),
+        .trainerror(NOT_TRAINERROR)
+    );
+
+    valtrainverf_vseq.configure(
+        .D2c_mode(SUCCESS),
+        .pattern_mode(PAT_ALL_LANES_VALID),
+        .data_mode(VALID_PATTERN),
+        .info_mode(CORRECT),
+        .message_mode(ALL_LANES_VALID),
+        .valid_mode(VALID_CORRECT),
+        .trainerror(NOT_TRAINERROR)
+    );
+    
+    DTC1_vseq.configure(
+        .D2c_mode(SUCCESS),
+        .pattern_mode(PAT_ALL_LANES_VALID),
+        .data_mode(LFSR_PATTERN),
+        .info_mode(CORRECT),
+        .message_mode(ALL_LANES_VALID),
+        .valid_mode(VALID_CORRECT)
+    );
+
+    datatrainvref_vseq.configure(
+        .D2c_mode(SUCCESS),
+        .pattern_mode(PAT_ALL_LANES_VALID),
+        .data_mode(LFSR_PATTERN),
+        .info_mode(CORRECT),
+        .message_mode(ALL_LANES_VALID),
+        .valid_mode(VALID_CORRECT)
+    );
+
+    DTC2_vseq.configure(
+        .D2c_mode(SUCCESS),
+        .pattern_mode(PAT_ALL_LANES_VALID),
+        .data_mode(LFSR_PATTERN),
+        .info_mode(CORRECT),
+        .message_mode(ALL_LANES_VALID),
+        .valid_mode(VALID_CORRECT)
+    );
+
+    LINKSPEED_vseq.configure(
+        .linkspeed_dest(LINKINIT)
+        ,.pattern_mode(PAT_ALL_LANES_VALID)
+        ,.message_mode(ALL_LANES_VALID)
+        ,.speed_idle_entry(CURRENT_DIE)
+    );
+
+
+    valtraincenter_vseq.start(p_sequencer);
+    valtrainverf_vseq.start(p_sequencer);
+    DTC1_vseq.start(p_sequencer);
+    datatrainvref_vseq.start(p_sequencer);
+    rxdskew_vseq.start(p_sequencer);
+    DTC2_vseq.start(p_sequencer);
+    LINKSPEED_vseq.start(p_sequencer);
+
+    wait_for_msg_ser_end();
+    // #1000ns;
+
+    wake_req_handshake.start(ltsm_rdi_seqr);
+    state_req_handshake.start(ltsm_rdi_seqr);
+    
+    p_sequencer.rx_fifo.get(sb_ltsm_item);
+    sb_ltsm_item.set_tx_encoding(sb_shared_pkg::ACTIVE_LINKINIT_STATE_REQ);
+    send_sb_msg(sb_ltsm_item);
+
+    p_sequencer.tx_fifo.get(sb_ltsm_item);
+    sb_ltsm_item.set_rx_encoding(sb_shared_pkg::ACTIVE_LINKINIT_STATE_RESP);
+    send_sb_msg(sb_ltsm_item);
+
+    wait_for_msg_ser_end();
+
+    fork
+        begin
+            active_tx_seq.start(tx_rdi_seqr);
+        end
+        begin
+            active_rx_seq.configure(
+                ._num_256b_chunks(2),
+                ._lane_map_code(X16_MODE),
+                ._scenario(ACTIVE_SCENARIO_IDEAL)
+            );
+            active_rx_seq.start(rp_rmblink_seqr);
+        end     
+    join_any
     
     wait_for_msg_ser_end();
     
